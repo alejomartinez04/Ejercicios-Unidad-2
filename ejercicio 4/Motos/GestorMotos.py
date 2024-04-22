7501e1fcd4be33e440f1fb07792d0bad9a3e29d8
alejomartinez04 <167478932+alejomartinez04@users.noreply.github.com>
2024-04-22 20:12:49 -0300

    Add files via upload

---
+++ ejercicio 4/Motos/GestorMotos.py
@@ -0,0 +1,32 @@
+from Motos.classMotos import Moto
+import csv
+class gestormotos:
+    __listamoto=list
+    def __init__(self):
+        self.__listamoto=[]
+    def agregarmoto(self,unamoto):
+        self.__listamoto.append(unamoto)
+    def leerdatos(self):
+        archivo=open('datosMoto.csv')
+        reader=csv.reader(archivo,delimiter=';')
+        bandera=True
+        for fila in reader:
+            if bandera==True:
+                bandera==False
+            else:
+                patente=fila[0]
+                marca=fila[1]
+                nya=fila[2]
+                kilom=fila[3]
+                unamoto=Moto(patente,marca,nya,kilom)
+                self.agregarmoto(unamoto)
+    def buscar(self,xpat):
+        indice=0
+        retorno=None
+        bandera=False
+        while not bandera and indice < len(self.__listamoto):
+            if self.__listamoto[indice].getpat()==xpat:
+                bandera=True
+            else:
+                indice+=1
+        return bandera
